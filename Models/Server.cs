630027225399d66d926929e20fa36b02d8e79f80
Oluwatosin Folaranmi <foluwatosin@ITILP_L014.simplexsystems.com>
2019-09-02 14:57:48 +0100

    Initial Commit

---
+++ Models/Server.cs
@@ -0,0 +1,9 @@
+namespace core-board.API.Models 
+{
+    public class Server
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool isOnline { get; set; }
+    }
+}
